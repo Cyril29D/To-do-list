eae239635d98871651e61c24e457b82ee5582fed
Cyril29D <cyrildelly9@gmail.com>
2025-07-31 16:34:46 +0200

    ajout du formulaire avec tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  <div className='h-screen bg-emerald-950'>
+    <div className="max-w-4xl mx-auto pt-20 px-6"> 
+<h1 className="text-3xl text-slate-100 mb-4">La To-do Liste </h1>
+    
+    <form className="mb-10">
+      <label htmlFor="todo-item" className="text-amber-50">Que souhaites-tu faire ?</label>
+      <input type="text" className="mt-1 block w-full rounded bg-amber-50" />
+      <button type="button" className="mt-4 py-2 px-2 bg-amber-50 rounded min-w-[115px]">Ajouter</button>
+    </form>
+    <ul>
+      
+    </ul>
+    </div>
+  </div>
   )
 }
 
