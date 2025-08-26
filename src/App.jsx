998b6cc17b47207b0b89de72bf87725e4911fc1d
Cyril29D <cyrildelly9@gmail.com>
2025-08-25 22:59:20 -0400

    Ajout de la validation

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,10 @@ import { nanoid } from "nanoid"
 import ListItems from "./components/ListItems";
 function App() {
 
-  const [todolist, setTodoList] = useState([
-    {id: nanoid(8), content: "item 1"},
-    {id: nanoid(8), content: "item 2"},
-    {id: nanoid(8), content: "item 3"},
-  ])
+  const [todolist, setTodoList] = useState("")
   const [todo, setTodo] = useState("")
 
+  const [showValidation, setShowValidation] = useState(false)
 
 function deleteTodo(id){
 setTodoList(todolist.filter(todo => todo.id !== id))
@@ -18,8 +15,13 @@ setTodoList(todolist.filter(todo => todo.id !== id))
   function handleSubmit(e){
   e.preventDefault()
 
+  if(todo === ""){
+    setShowValidation(true)
+    return
+  }
   setTodoList([...todolist, {id: nanoid(), content: todo}])
   setTodo("")
+  setShowValidation(false)
 }
   return (
   <div className='h-screen bg-emerald-950'>
@@ -33,7 +35,10 @@ setTodoList(todolist.filter(todo => todo.id !== id))
       onChange={e => setTodo(e.target.value)}
       type="text" 
        className="mt-1 block w-full rounded bg-amber-50" />
-    
+    {showValidation && (
+      <p className="text-red-400"> Vous n'avez rien ajouter 
+      </p>
+    )}
       <button  className="mt-4 py-2 px-2 bg-amber-50 rounded min-w-[115px] hover:bg-blue-600">Ajouter</button>
  
     </form>
